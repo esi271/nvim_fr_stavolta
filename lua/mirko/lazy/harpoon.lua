580ea33589102c1b3b585a66c6e63438731eb405
esi271 <jmpanganiban314@gmail.com>
2025-11-07 18:22:50 +0100

    amogu

---
+++ lua/mirko/lazy/harpoon.lua
@@ -0,0 +1,17 @@
+return {
+    "ThePrimeagen/harpoon",
+    config = function()
+	require("harpoon").setup()
+
+	local mark = require("harpoon.mark")
+	local ui = require("harpoon.ui")
+
+	vim.keymap.set("n", "<leader>a", mark.add_file)
+	vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
+
+	vim.keymap.set("n", "<C-n>", function() ui.nav_next() end)
+	vim.keymap.set("n", "<C-p>", function() ui.nav_prev() end)
+
+
+    end,
+}
