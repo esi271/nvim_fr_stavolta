084caccd50c9de96a1bbddaedc95e80ed991aa12
mirko <johnmirko.panganiban@studenti.unimi.it>
2025-11-09 11:32:10 +0100

    altro

--- lua/mirko/lsp.lua
+++ lua/mirko/lsp.lua
@@ -1,2 +1,9 @@
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("jdtls")
+vim.lsp.enable("rust_analyzer")
+
+vim.lsp.config('bash-language-server', {
+    filetypes = { "sh", "zsh" },
+})
+
+vim.lsp.enable("bash-language-server")
